10a3ca29dcbf98cbc35d3a45abe917711ae39d8f
Kjeld Ritmeester <99071969@mydavinci.nl>
2023-03-07 13:13:06 +0100

    updates en rotzooi

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+tekst = localStorage.getItem(("tekst"))
+console.log(tekst)
+if (tekst == "1")
+{
+    localStorage.setItem("tekst","2")
+    document.getElementById("welkom").innerText = ("Hoi, welkom!")
+}
+else if (tekst =="2")
+{
+    localStorage.setItem("tekst","3")
+    document.getElementById("welkom").innerText = ("Welkom op mijn website!")
+}
+else if (tekst =="3")
+{
+    localStorage.setItem("tekst","1")
+    document.getElementById("welkom").innerText = ("Hallo en welkom!")
+}
+else
+{
+    localStorage.setItem("tekst","1")
+}
 function klik()
 {
     alert("test")
-}
+};
